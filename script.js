b3c373aa0756cfa6ce76a088ce89a51c79a82a9d
viet47794-cell <viet47794@gmail.com>
2026-05-14 19:52:37 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
+// Đổi giao diện
+const btn = document.getElementById('theme-toggle');
+const body = document.body;
 
+btn.addEventListener('click', () => {
+    const currentTheme = body.getAttribute('data-theme');
+    const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+    body.setAttribute('data-theme', newTheme);
+    
+    // Đổi icon cho vui mắt (Hoa anh đào/Bông tuyết)
+    btn.textContent = newTheme === 'dark' ? '🌸' : '❄️';
+});
+
+// Accordion đóng mở kỹ năng
+const accordions = document.querySelectorAll('.accordion-btn');
+
+accordions.forEach(acc => {
+    acc.addEventListener('click', function() {
+        this.classList.toggle('active');
+        const content = this.nextElementSibling;
+        
+        if (content.classList.contains('show')) {
+            content.classList.remove('show');
+        } else {
+            content.classList.add('show');
+        }
+    });
+});
